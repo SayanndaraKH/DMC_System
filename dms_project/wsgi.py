871b58025382b2b0ba610ae685c3098e2221394f
SayanndaraKH <sayanndara2022@gmail.com>
2026-08-25 17:52:00 +0700

    Add sys.path guarantees and push to main and master

--- dms_project/wsgi.py
+++ dms_project/wsgi.py
@@ -1,4 +1,11 @@
 import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+if str(BASE_DIR) not in sys.path:
+    sys.path.insert(0, str(BASE_DIR))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dms_project.settings')
